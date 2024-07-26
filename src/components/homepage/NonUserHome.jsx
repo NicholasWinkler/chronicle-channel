5659d0374be0a251555808cfb3db6d413ba79354
Nicholas Winkler <nhwinkler@gmail.com>
2024-07-26 16:11:55 -0500

    Created buttons for NonUserHomepage

--- src/components/homepage/NonUserHome.jsx
+++ src/components/homepage/NonUserHome.jsx
@@ -1,14 +1,22 @@
+import { Link } from "react-router-dom";
 import "./homepage.css";
 
 export const NonUserHome = () => {
-        return (
-          <div className="homepage-container">
-            <h1>
-              <span>Welcome to</span>
-              <span>The Chronicle Channel</span>
-            </h1>
-            <div>Map Out Events, One Timeline at a Time</div>
-          </div>
-        );
-      };
-      
+  return (
+    <div className="homepage-container">
+      <h1>
+        <span>Welcome to</span>
+        <span>The Chronicle Channel</span>
+      </h1>
+      <h3>Map Out Events, One Timeline at a Time</h3>
+      <div>
+        <Link to="/register">
+          <button className="btn-primary">Register</button>
+        </Link>
+        <Link to="/login">
+          <button className="btn-primary">Login</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
